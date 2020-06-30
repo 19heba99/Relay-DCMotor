9fa17d70e7cb6386f6743dede347618f95d6e02b
Heba Jamal <66881972+19heba99@users.noreply.github.com>
2020-06-30 22:29:57 +0300

    Update RelayDC.ino

--- RelayDC.ino
+++ RelayDC.ino
@@ -1,14 +1,43 @@
 
 void setup() {
   
-  pinMode(8, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
 }
 
 void loop() {
 
-  digitalWrite(8, 0);
+if (digitalRead(7) == 0 && digitalRead(11) == 1) {
+  digitalWrite(1, 1);
+  digitalWrite(13, 0);
   delay(1000);
+
+}
+
+else if (digitalRead(7) == 1 && digitalRead(11) == 0) {
+
+  digitalWrite(1, 0);
+  digitalWrite(13, 1);
+  delay(1000);
+}
+
+else if (digitalRead(7) == 0 && digitalRead(1) == 0) {
+
+  digitalWrite(1, 1);
+  digitalWrite(13, 0);
+  delay(2000);
   
-  digitalWrite(8, 1);
+  digitalWrite(1, 0);
+  digitalWrite(13, 1);
+  delay(2000);
+}
+
+else { 
+
+  digitalWrite(1, 0);
+  digitalWrite(13, 0);
   delay(1000);
+}
 }
